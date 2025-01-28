64c0b75f009f12efc7d5260167cb258001b2a2b7
MaxDnipro <maximlisogor@gmail.com>
2025-01-28 18:21:41 +0200

    created file main.js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+"use strict"
+
+let onBox = "Tad Lasso";
+
+console.log(onBox, typeof onBox);
+
+
+
+let table = 1;
+
+console.log(table, typeof table);
+
+
+let mirror = true;
+
+console.log(mirror, typeof mirror);
+
+
+
+const lamp = {
+  lampochka: true,
+  provodLenghtMeter: 3,
+  podstavka: 'circle',
+  shtativ: 'metall',
+};
+
+console.log(lamp);
+
+
+
+const furnitureRoom = [
+  "sofa",
+  "kreslo",
+  "stul",
+  "polka"
+];
+
+console.log(furnitureRoom);
+
